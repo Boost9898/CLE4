7247ddeef59bb416afb44281631db6aa96cfc448
Bram <bls.place@gmail.com>
2020-05-11 16:32:39 +0200

    Feestje

--- js/main.js
+++ js/main.js
@@ -1,12 +1,34 @@
-//
-// vis element
-//
-let fish = document.getElementsByTagName("fish")[0]
-fish.style.transform = "translate(200px, 100px)"
-fish.style.filter = "hue-rotate(200deg)"
+function randomMinMax(min, max) {
+    som = Math.random() * (max - min) + min
+    return Math.floor(som)
+}
 
-//
-// bubble element
-//
-let bubble = document.getElementsByTagName("bubble")[0]
-bubble.style.transform = "translate(20vw, 100vh)"
+function dead() {
+    console.log("dead test")
+}
+
+
+function spawn() {
+    //
+    // vis element
+    //
+    let fish = document.createElement("fish")
+    document.getElementById("bg").appendChild(fish)
+    fish.addEventListener(onclick, (dead())) //waarom logt hij meteen de 'dead test' en komt die niet pas bij de klik?
+
+    fish.style.transform = `translate(${randomMinMax(0, 1750)}px, ${randomMinMax(0, 850)}px)` //verticaal horizontaal
+    fish.style.filter = `hue-rotate(${randomMinMax(0, 360)}deg)`
+
+
+    //
+    // bubble element
+    //
+    let bubble = document.createElement("bubble")
+    document.getElementById("bg").appendChild(bubble)
+    bubble.style.transform = `translate(${randomMinMax(0, 1800)}px, ${randomMinMax(0, 800)}px)` //verticaal horizontaal
+};
+
+
+for (let index = 0; index <= 99; index++) {
+    spawn();
+}
