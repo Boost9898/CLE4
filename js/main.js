112a94ea5e73ea41b570e8e685da697e720eaaa7
Bram <bls.place@gmail.com>
2020-05-12 15:00:28 +0200

    Uitleg

--- js/main.js
+++ js/main.js
@@ -1,34 +1,39 @@
-function randomMinMax(min, max) {
-    som = Math.random() * (max - min) + min
-    return Math.floor(som)
+let game = document.getElementsByTagName("game")[0]
+
+for (let index = 0; index <= 99; index++) {
+    spawnFish();
+    spawnBubble();
 }
 
-function dead() {
-    console.log("dead test")
-}
-
-
-function spawn() {
-    //
-    // vis element
-    //
+function spawnFish() {
     let fish = document.createElement("fish")
-    document.getElementById("bg").appendChild(fish)
-    fish.addEventListener(onclick, (dead())) //waarom logt hij meteen de 'dead test' en komt die niet pas bij de klik?
+    game.appendChild(fish)
+    fish.addEventListener("click", onFishClick)
 
-    fish.style.transform = `translate(${randomMinMax(0, 1750)}px, ${randomMinMax(0, 850)}px)` //verticaal horizontaal
-    fish.style.filter = `hue-rotate(${randomMinMax(0, 360)}deg)`
+    let x = Math.random() * (window.innerWidth - fish.clientWidth)
+    let y = Math.random() * (window.innerHeight - fish.clientHeight)
+    let color = Math.random() * 360
 
-
-    //
-    // bubble element
-    //
-    let bubble = document.createElement("bubble")
-    document.getElementById("bg").appendChild(bubble)
-    bubble.style.transform = `translate(${randomMinMax(0, 1800)}px, ${randomMinMax(0, 800)}px)` //verticaal horizontaal
+    fish.style.transform = `translate(${x}px, ${y}px)`
+    fish.style.filter = `hue-rotate(${color}deg)`
 };
 
 
-for (let index = 0; index <= 99; index++) {
-    spawn();
+function spawnBubble() {
+    let bubble = document.createElement("bubble")
+    game.appendChild(bubble)
+    bubble.addEventListener("click", onBubbleClick)    
+
+    let x = Math.random() * (window.innerWidth - bubble.clientWidth)
+    let y = Math.random() * (window.innerHeight - bubble.clientHeight)
+
+    bubble.style.transform = `translate(${x}px, ${y}px)`
+};
+
+function onFishClick(event) {
+    event.target.classList.add("dead")
+}
+
+function onBubbleClick(event) {
+    event.target.remove()
 }
